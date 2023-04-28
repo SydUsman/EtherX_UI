6a2f52f3b822f85fe820203e49b05ed4a52931f6
usmanhassannaqvi <usmanhassannaqvi@gmail.com>
2023-04-28 22:50:36 +0500

    secong commit

--- src/components/GigCard/GigCard.jsx
+++ src/components/GigCard/GigCard.jsx
@@ -1,38 +1,77 @@
-import "./GigCard.scss";
-import { Link } from "react-router-dom";
-import { useQuery } from "@tanstack/react-query";
-import newRequest from "../../utils/newRequest";
+// import "./GigCard.scss";
+// import { Link } from "react-router-dom";
+// import { useQuery } from "@tanstack/react-query";
+// import newRequest from "../../utils/newRequest";
+// import React from "react";
+// const GigCard = ( item ) => {
+//   const { isLoading, error, data } = useQuery({
+//     queryKey: [item.userId],
+//     queryFn: () =>
+//       newRequest.get(`/users/${item.userId}`).then((res) => {
+//         return res.data;
+//       }),
+//   });
+//   return (
+//     <Link to={`/gig/${item._id}`} className="link">
+//       <div className="gigCard">
+//         <img src={item.cover} alt="" />
+//         <div className="info">
+//           {isLoading ? (
+//             "loading"
+//           ) : error ? (
+//             "Something went wrong!"
+//           ) : (
+//             <div className="user">
+//               <img src={data.img || "/images/noavatar.jpg"} alt="" />
+//               <span>{data.username}</span>
+//             </div>
+//           )}
+//           <p>{item.desc}</p>
+//           <div className="star">
+//             <img src="./images/star.png" alt="" />
+//             <span>
+//               {!isNaN(item.totalStars / item.starNumber) &&
+//                 Math.round(item.totalStars / item.starNumber)}
+//             </span>
+//           </div>
+//         </div>
+//         <hr />
+//         <div className="detail">
+//           <img src="./images/heart.png" alt="" />
+//           <div className="price">
+//             <span>STARTING AT</span>
+//             <h2>$ {item.price}</h2>
+//           </div>
+//         </div>
+//       </div>
+//     </Link>
+//   );
+// };
+
+// export default GigCard;
+
+//You can use below code its just the UI Above code functionality is not completed by usman 
+
 import React from "react";
-const GigCard = ( item ) => {
-  const { isLoading, error, data } = useQuery({
-    queryKey: [item.userId],
-    queryFn: () =>
-      newRequest.get(`/users/${item.userId}`).then((res) => {
-        return res.data;
-      }),
-  });
+import { useEffect } from "react";
+import "./GigCard.scss";
+import { Link} from "react-router-dom";
+
+const GigCard = ({ item }) => {
+
   return (
-    <Link to={`/gig/${item._id}`} className="link">
+    <Link to="/gig/123" className="link">
       <div className="gigCard">
-        <img src={item.cover} alt="" />
+        <img src={item.img} alt="" />
         <div className="info">
-          {isLoading ? (
-            "loading"
-          ) : error ? (
-            "Something went wrong!"
-          ) : (
-            <div className="user">
-              <img src={data.img || "/images/noavatar.jpg"} alt="" />
-              <span>{data.username}</span>
-            </div>
-          )}
+          <div className="user">
+            <img src={item.pp} alt="" />
+            <span>{item.username}</span>
+          </div>
           <p>{item.desc}</p>
           <div className="star">
             <img src="./images/star.png" alt="" />
-            <span>
-              {!isNaN(item.totalStars / item.starNumber) &&
-                Math.round(item.totalStars / item.starNumber)}
-            </span>
+            <span>{item.star}</span>
           </div>
         </div>
         <hr />
@@ -40,7 +79,10 @@ const GigCard = ( item ) => {
           <img src="./images/heart.png" alt="" />
           <div className="price">
             <span>STARTING AT</span>
-            <h2>$ {item.price}</h2>
+            <h2>
+              $ {item.price}
+              <sup>99</sup>
+            </h2>
           </div>
         </div>
       </div>
--- src/pages/Gigs Categories/Gigs.jsx
+++ src/pages/Gigs Categories/Gigs.jsx
@@ -1,47 +1,126 @@
-import React, { useEffect, useRef, useState } from "react";
+// import React, { useEffect, useRef, useState } from "react";
+// import "./Gigs.scss";
+// import GigCard from "../../components/GigCard/GigCard";
+// import { useQuery } from "@tanstack/react-query";
+// import newRequest from "../../utils/newRequest";
+// import { useLocation } from "react-router-dom";
+
+// function Gigs() {
+//   const [sort, setSort] = useState("sales");
+//   const [open, setOpen] = useState(false);
+//   const minRef = useRef();
+//   const maxRef = useRef();
+//   useEffect(() => {
+//     window.scrollTo(0, 0);
+//   }, []);
+
+//   const { search } = useLocation();
+
+//   const { isLoading, error, data, refetch } = useQuery({
+//     queryKey: ["gigs"],
+//     queryFn: () =>
+//       newRequest
+//         .get(
+//           `/gigs${search}&min=${minRef.current.value}&max=${maxRef.current.value}&sort=${sort}`
+//         )
+//         .then((res) => {
+//           return res.data;
+//         }),
+//   });
+
+//   console.log(data);
+
+//   const reSort = (type) => {
+//     setSort(type);
+//     setOpen(false);
+//   };
+
+//   useEffect(() => {
+//     refetch();
+//   }, [sort]);
+
+//   const apply = () => {
+//     refetch();
+//   };
+
+//   return (
+//     <div className="gigs">
+//       <div className="container">
+//         <span className="breadcrumbs">EtherX - Graphics & Design -</span>
+//         <h1>AI Artists</h1>
+//         <p>
+//           Explore the boundaries of art and technology with EtherX AI artists
+//         </p>
+//         <div className="menu">
+//           <div className="left">
+//             <span>Budget</span>
+//             <input ref={minRef} type="number" placeholder="min" />
+//             <input ref={maxRef} type="number" placeholder="max" />
+//             <button onClick={apply}>Apply</button>
+//           </div>
+//           <div className="right">
+//             <span className="sortBy">Sort by</span>
+//             <span className="sortType">
+//               {sort === "sales" ? "Best Selling" : "Newest"}
+//             </span>
+//             <img
+//               src="./images/down.png"
+//               alt=""
+//               onClick={() => setOpen(!open)}
+//             />
+//             {open && (
+//               <div className="rightMenu">
+//                 {sort === "sales" ? (
+//                   <span onClick={() => reSort("createdAt")}>Newest</span>
+//                 ) : (
+//                   <span onClick={() => reSort("sales")}>Best Selling</span>
+//                 )}
+//                 <span onClick={() => reSort("sales")}>Popular</span>
+//               </div>
+//             )}
+//           </div>
+//         </div>
+//         <div className="cards">
+//           {isLoading
+//             ? "loading"
+//             : error
+//             ? "Something went wrong!"
+//             : data.map((gig) => <GigCard key={gig._id} item={gig} />)}
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Gigs;
+
+
+// //You can use below code its just the UI Above code functionality is not completed by usman 
+
+import React, { useRef, useState } from "react";
 import "./Gigs.scss";
+import { gigs } from "../../data";
 import GigCard from "../../components/GigCard/GigCard";
-import { useQuery } from "@tanstack/react-query";
-import newRequest from "../../utils/newRequest";
-import { useLocation } from "react-router-dom";
+import { useEffect } from "react";
 
 function Gigs() {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
   const [sort, setSort] = useState("sales");
   const [open, setOpen] = useState(false);
   const minRef = useRef();
   const maxRef = useRef();
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, []);
-
-  const { search } = useLocation();
-
-  const { isLoading, error, data, refetch } = useQuery({
-    queryKey: ["gigs"],
-    queryFn: () =>
-      newRequest
-        .get(
-          `/gigs${search}&min=${minRef.current.value}&max=${maxRef.current.value}&sort=${sort}`
-        )
-        .then((res) => {
-          return res.data;
-        }),
-  });
-
-  console.log(data);
 
   const reSort = (type) => {
     setSort(type);
     setOpen(false);
   };
 
-  useEffect(() => {
-    refetch();
-  }, [sort]);
-
-  const apply = () => {
-    refetch();
-  };
+  const apply = ()=>{
+    console.log(minRef.current.value)
+    console.log(maxRef.current.value)
+  }
 
   return (
     <div className="gigs">
@@ -63,29 +142,23 @@ function Gigs() {
             <span className="sortType">
               {sort === "sales" ? "Best Selling" : "Newest"}
             </span>
-            <img
-              src="./images/down.png"
-              alt=""
-              onClick={() => setOpen(!open)}
-            />
+            <img src="./images/down.png" alt="" onClick={() => setOpen(!open)} />
             {open && (
               <div className="rightMenu">
                 {sort === "sales" ? (
                   <span onClick={() => reSort("createdAt")}>Newest</span>
                 ) : (
                   <span onClick={() => reSort("sales")}>Best Selling</span>
-                )}
-                <span onClick={() => reSort("sales")}>Popular</span>
+                  )}
+                  {/* <span onClick={() => reSort("sales")}>Popular</span> */}
               </div>
             )}
           </div>
         </div>
         <div className="cards">
-          {isLoading
-            ? "loading"
-            : error
-            ? "Something went wrong!"
-            : data.map((gig) => <GigCard key={gig._id} item={gig} />)}
+          {gigs.map((gig) => (
+            <GigCard key={gig.id} item={gig} />
+          ))}
         </div>
       </div>
     </div>
